931ffa99186b8a741426ebe22667c373190a0162
tommyod <tommy.odland@gmail.com>
2023-09-02 21:17:43 +0200

    require sklearn 1.3.0

--- docs/examples/diagnostic_plots.py
+++ docs/examples/diagnostic_plots.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.4
+#       jupytext_version: 1.14.5
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -77,3 +77,5 @@ display = QQDisplay.from_estimator(
 ax.grid(True)
 fig.tight_layout()
 plt.show()
+
+# %%
